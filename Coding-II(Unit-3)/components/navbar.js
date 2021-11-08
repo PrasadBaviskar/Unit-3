9f9bef21b0e8e9297e08c4d24f4e978cac1e4d23
Prasad Baviskar <71400522+PrasadBaviskar@users.noreply.github.com>
2021-11-08 14:25:35 +0530

    add navbar

---
+++ Coding-II(Unit-3)/components/navbar.js
@@ -0,0 +1,12 @@
+function navbar(){
+    return `<div> <a href="index.html"> Home </a> </div>
+
+    <div class="options">
+        <div> <a href="#"> Menu </a>  </div>
+        <div> <a href="signup.html"> Sign Up </a> </div>
+        <div> <a href="signin.html"> Sign In </a> </div>
+    </div>`
+}
+
+
+export default navbar
